62b659bc005a69c5dfe75cb0e69a8ca25e96b4cd
mrbrokenml@gmail.com <mrbrokenml@gmail.com>
2026-01-27 16:44:17 +0530

    updated full routing

--- app.py
+++ app.py
@@ -12,7 +12,7 @@ def home():
     
 @app.route('/health')
 def health():
-    return "OK",200
+    return render_template("health.html")
  
 if __name__=="__main__":
     app.run(host="0.0.0.0", port=5000)
