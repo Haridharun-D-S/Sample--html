a7fb80d1f0c6a0fd0c606042fda44b6aa35c5c37
mrbrokenml@gmail.com <mrbrokenml@gmail.com>
2026-01-27 11:38:45 +0530

    Run py flask

--- app.py
+++ app.py
@@ -1,3 +1,14 @@
-for i in range(0,5,1):
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route("/")
+def home():
+    for i in range(0,5,1):
     print("Hello",i,end="\n")
     print("ok")
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)
+
+
